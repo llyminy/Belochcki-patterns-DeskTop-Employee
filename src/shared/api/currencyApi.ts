73a24b1abd0f9b3e87834bd870a90885980f33d2
A0l1i1c3e4 <vas120704@gmail.com>
2026-03-23 00:38:02 +0700

    Code update

--- src/shared/api/currencyApi.ts
+++ src/shared/api/currencyApi.ts
@@ -3,17 +3,17 @@ import type { Currency, CreateCurrencyRequest} from "../../types/Currency";
 
 export const currencyApi = {
   getAll: async (): Promise<Currency[]> => {
-    const res = await http.get<Currency[]>("/currencies");
+    const res = await http.get<Currency[]>("/gateway/accounts/currencies");
     return res;
   },
 
   create: async (data: CreateCurrencyRequest): Promise<Currency> => {
-    const res = await http.post<Currency>("/currencies", data);
+    const res = await http.post<Currency>("/gateway/accounts/currencies", data);
     return res;
   },
 
   deactivate: async (code: string): Promise<Currency> => {
-    const res = await http.patch<Currency>(`/currencies/${code}/deactivate`);
+    const res = await http.put<Currency>(`/gateway/accounts/currencies/${code}/deactivate`);
     return res;
   },
 };
